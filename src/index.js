e5549bfe0780b59123c43f5456dbe024a7fe2748
Thiago <thiagocordeirodanttas@gmail.com>
2023-03-17 21:19:22 -0300

    comecando as classes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const personagemPedrinho = {
-    nome: 'Pedrinho',
-    vida: 7,
-    mana: 12,
-    level: 5,
-    tipo: 'Mago',
-}
+// const personagemPedrinho = {
+//     nome: 'Pedrinho',
+//     vida: 7,
+//     mana: 12,
+//     level: 5,
+//     tipo: 'Mago',
+// }
 
 const personagemJose = {
     nome: 'Jose',
@@ -14,3 +14,28 @@ const personagemJose = {
     tipo: 'Arqueiro',
 }
 
+const personagemThiago = {
+    nome: 'Thiago',
+    vida: 10,
+    mana: 5,
+    level: 30,
+    tipo : 'Cavaleiro'
+}
+
+class Personagem {
+    nome
+    vida
+    mana
+    level
+    tipo
+    descricao
+}
+
+const personagemPedrinho = new Personagem();
+
+personagemPedrinho.nome = "Pedrinho"
+personagemPedrinho.mana = 5
+personagemPedrinho.vida = 10
+personagemPedrinho.level = 3
+personagemPedrinho.tipo  = "Cavaleiro"
+personagemPedrinho.descricao = "Cavaleiro destruidor"
