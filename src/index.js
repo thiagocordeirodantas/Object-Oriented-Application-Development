92a30ae13ea4879cf2c2c03bba92bea77b566e06
Thiago Cordeiro <twthicordeirodantas@gmail.com>
2023-03-20 17:10:28 -0300

    criando a classe mago

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Personagem from "./modules/personagem.js";
 import { PersonagemView } from "./components/personagem-view.js";
+import Mago from "./modules/mago.js";
 
 const personagemThiago = new Personagem('Thiago', 5, 'Mago')
 const personagemJose = new Personagem('Jose', 2 , 'Arqueiro')
@@ -7,3 +8,4 @@ const personagemJose = new Personagem('Jose', 2 , 'Arqueiro')
 const personagens =  [personagemJose, personagemThiago]
 
 new PersonagemView(personagens).render()
+
---
+++ src/modules/mago.js
@@ -0,0 +1,8 @@
+export default class Mago {
+    constructor(elementoMagico,levelMagico,inteligencia){
+
+        this.elementoMagico = elementoMagico
+        this.levelMagico = levelMagico
+        this.inteligencia = inteligencia
+    }
+}
