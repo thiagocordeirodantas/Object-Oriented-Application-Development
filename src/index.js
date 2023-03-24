ab2e0c7cc0b048bb0cd52fc1c07bc4d9c5a9867e
Thiago Cordeiro <twthicordeirodantas@gmail.com>
2023-03-23 21:27:19 -0300

    comecando criar os vencedores

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import ArqueiroMago from "./modules/arqueiro-mago.js";
 
 
 const arqueiroMaaago = new ArqueiroMago('Joao Matias',230,122,'ar',121,80)
-const magolouco = new Mago('Thiago',32,'gelo',3,3)
+const magolouco = new Mago('Thiago',333,'gelo',3,3)
 
 const personagens =  [arqueiroMaaago,magolouco]
 
@@ -15,3 +15,5 @@ const personagens =  [arqueiroMaaago,magolouco]
 
 new PersonagemView(personagens).render()
 
+
+console.log(Personagem.verificarVencedor(arqueiroMaaago,magolouco))
--- src/modules/personagem.js
+++ src/modules/personagem.js
@@ -17,4 +17,14 @@ export default class Personagem {
         }
         return `${this.constructor.tipo} Iniciante`
     }
+
+    static verificarVencedor(personagem1,personagem2){
+        if(personagem1.level  === personagem2.level){
+            return 'Empate'
+        } 
+        if(personagem1.level > personagem2.level) {
+            return `${personagem1.constructor.tipo} ${personagem1.nome} e o vencedor`
+        }
+        return `${personagem2.constructor.tipo} ${personagem2.nome} e o vencedor`
+    }
 }
