59582dd1fbb626f3f70cbbbaa97391cf1ac51694
Thiago Cordeiro <twthicordeirodantas@gmail.com>
2023-03-23 21:47:22 -0300

    encapsulando

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ArqueiroMago from "./modules/arqueiro-mago.js";
 
 const arqueiroMaaago = new ArqueiroMago('Joao Matias',230,122,'ar',121,80)
 const magolouco = new Mago('Thiago',333,'gelo',3,3)
-
+//arqueiroMaaago.#level = 15
 const personagens =  [arqueiroMaaago,magolouco]
 
 
@@ -15,5 +15,5 @@ const personagens =  [arqueiroMaaago,magolouco]
 
 new PersonagemView(personagens).render()
 
-
-console.log(Personagem.verificarVencedor(arqueiroMaaago,magolouco))
+console.log(arqueiroMaaago)
+//console.log(Personagem.verificarVencedor(arqueiroMaaago,magolouco))
